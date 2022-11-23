6d34fd96c647b4417fb9ef0c231a9bb004d2ac84
Mariano Koldobsky <mkoldobsky@gmail.com>
2022-11-23 09:55:11 -0300

    fix typo on constructor

--- javascript/src/account.js
+++ javascript/src/account.js
@@ -1,12 +1,11 @@
 import Balance from "./balance";
 
 export default class Account {
-    contructor(){
-        this.balance = new Balance(0);
-    }
+  constructor() {
+    this.balance = new Balance(0);
+  }
   balanceEqualTo(otherBalance) {
     return otherBalance.equalTo(this.balance);
-
   }
 
   deposit(amount) {
